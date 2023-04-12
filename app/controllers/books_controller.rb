d9ed22e25f7663d22a71bebc56114cb99a78c8a4
EC2 Default User <ec2-user@ip-172-31-6-65.ap-northeast-1.compute.internal>
2023-04-12 13:04:40 +0000

    regular updates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,46 @@ class BooksController < ApplicationController
   
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    #book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    #book.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    #redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      #flash[:notice] = "Book was successfully created"
+      #redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.all 
+    #indexにnew（投稿画面）を書き加えたので、下記追加
+    @book = Book.new  
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  #/でトップページを表示できるようにする
+  root 'homes#top'
   resources :homes
-  resources :books#, except: [:new, :index, :show, :edit]
+  resources :books
   #get 'books/new'
   #get 'books/index'
   #get 'books/show'
   #get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
+  #get 'top' => 'homes#top'
+  #試し　post 'books' => 'books#create'
+  #試し　get 'books' => 'books#index'
+  #as: 'book'にすると、コントローラーのcreateのredirectで、他の設定と干渉するため、
+  #'booka'に設定
+  #試し　get 'books/:id' => 'books#show', as: 'booka'
+  #as: 'edit_book'にすると、他の設定と干渉するため、'edit_bookb'に設定
+  #試し　get 'books/:id/edit' => 'books#edit', as: 'edit_bookb'
+  #試し　patch 'books/:id' => 'books#update', as: 'update_book'
+  #試し　delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
